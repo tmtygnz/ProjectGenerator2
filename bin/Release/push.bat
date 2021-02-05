5cce6ea750b547955ceb6d18ada9e1d5bd9056d5
Timoy_7207 <jostimian@gmail.com>
2021-02-05 21:26:43 +0800

    GIF edited

--- bin/Release/push.bat
+++ bin/Release/push.bat
@@ -1,7 +1,7 @@
-cd C:\Users\josti\Desktop\Projects\new test proj
+cd C:\Users\josti\Desktop\Projects\Test
 git init
 git add --all
 git commit -m "initial commit"
 git branch -M main
-git remote add origin https://github.com/jostimian/Test.git
+git remote add origin https://github.com/jostimian/Test2.git
 git push -u origin main
